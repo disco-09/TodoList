b1d10c7b4d61b5375bb1f52a1f9104ee492e7023
disco-09 <discoadapala@gmail.com>
2025-08-19 16:17:03 +0530

    Fixed vite base and switched to HashRouter

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,24 +1,17 @@
-import React from "react";
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import "./App.css";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
 
 function App() {
-  const token = localStorage.getItem("token");
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route
-          path="/dashboard"
-          element={token ? <Dashboard /> : <Navigate to="/login" />}
-        />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
