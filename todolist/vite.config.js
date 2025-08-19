b7e2e646f7d7162122f4a0041546dcd3a82c175e
disco-09 <discoadapala@gmail.com>
2025-08-19 15:09:57 +0530

    Update vite.config.js for GitHub Pages

--- todolist/vite.config.js
+++ todolist/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // HashRouter handles GitHub Pages, no need for repo name here
+  base: '/TodoList/', // repo name here
 });
