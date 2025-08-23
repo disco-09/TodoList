fcca66f99c1e5bb82b568e92a54bc2891ed6472f
disco-09 <discoadapala@gmail.com>
2025-08-24 01:18:57 +0530

    update source files

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,19 +1,12 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import "./App.css";  // ✅ add this line
+import "./App.css";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        {/* Open Home first */}
         <Route path="/" element={<Home />} />
-
-        {/* Other pages */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
